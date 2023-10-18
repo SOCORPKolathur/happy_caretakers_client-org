64bc72275b8a583395f5eabe9da98d13881ec9db
SO CORP 02 <socorpkolathur@gmail.com>
2023-10-18 21:54:28 +0530

    @Correction by Nagaraj.K

--- lib/views/products_view.dart
+++ lib/views/products_view.dart
@@ -113,7 +113,7 @@ class _ProductsViewState extends State<ProductsView> {
                       price: (i + 1) * 100.0,
                     );
                   },
-                ),
+                ), 
               ),
             )
           ],
