3eba041d56e0bc0faaef2729a7b05b57b8539e23
SO CORP 02 <socorpkolathur@gmail.com>
2023-11-16 18:58:28 +0530

    @Correction by Nagaraj.K

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:happy_caretakers_client/views/intro_view.dart';
-
 import 'firebase_options.dart';
 
 void main() async {
@@ -14,10 +13,11 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Happy Caretakers',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
